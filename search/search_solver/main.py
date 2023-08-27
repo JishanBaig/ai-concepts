448d2e51e89f99ae23cbd1cb339fc29b5e6e2b90
Jishan Baig <jishan@Jishans-MacBook-Pro.local>
2023-08-28 01:13:37 +0530

    more-maze-examples

--- search/search_solver/main.py
+++ search/search_solver/main.py
@@ -4,15 +4,30 @@ from strategies import DFS, BFS
 from visualizer import Visualizer
 
 
-def main():
-    space = MazeSearchSpace("maze.txt")
+def solveByDfs(datafile):
+    space = MazeSearchSpace(datafile)
     strategy = DFS()
     solution = strategy.search(space)
     Visualizer.visualize(space, solution)
 
+
+def solveByBfs(datafile):
+    space = MazeSearchSpace(datafile)
     strategy = BFS()
     solution = strategy.search(space)
     Visualizer.visualize(space, solution)
+
+
+def solve(datafile):
+    solveByDfs(datafile)
+    solveByBfs(datafile)
+
+
+def main():
+    solve("Data/maze.txt");
+    solve("Data/maze1.txt");
+    solve("Data/maze2.txt");
+    solve("Data/maze3.txt");
 
 
 if __name__ == "__main__":
